88dc29d3dba3e7459fe6981eb423d574ac0547f3
Anton Panteleev <a.s.panteleev@yandex.ru>
2025-12-08 14:47:56 +0300

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,8 +1,9 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()  # Создаем новый контекст браузера (новая сессия, которая изолирована от других)
+    page = context.new_page()  # Открываем новую страницу в рамках контекста
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
@@ -18,6 +19,18 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    dashboard_toolbar_title = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_toolbar_title).to_be_visible()
-    expect(dashboard_toolbar_title).to_have_text("Dashboard")
+    # dashboard_toolbar_title = page.get_by_test_id('dashboard-toolbar-title-text')
+    # expect(dashboard_toolbar_title).to_be_visible()
+    # expect(dashboard_toolbar_title).to_have_text("Dashboard")
+
+    # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
+    context.storage_state(path="browser-state.json")
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")  # Указываем файл с сохраненным состоянием
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    page.wait_for_timeout(5000)
