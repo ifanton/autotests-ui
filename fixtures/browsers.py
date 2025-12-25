98d97b840ddb83f9db37220fcf5d70175165a40f
Anton Panteleev <a.s.panteleev@yandex.ru>
2025-12-25 16:30:28 +0300

    Практикуемся в реализации PageComponent

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,5 +1,5 @@
 import pytest
-from playwright.sync_api import Playwright, Page
+from playwright.sync_api import Playwright, Page, ViewportSize
 
 
 @pytest.fixture
@@ -36,6 +36,9 @@ def initialize_browser_state(playwright: Playwright):
 @pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json")
+    context = browser.new_context(
+        storage_state="browser-state.json",
+        viewport=ViewportSize({"width": 1920, "height": 1080})
+    )
     yield context.new_page()
     browser.close()
