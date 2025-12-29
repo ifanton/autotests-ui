9d1c2b00b21988f2c1a797eaa85e98d6715fc285
Anton Panteleev <a.s.panteleev@yandex.ru>
2025-12-29 17:14:11 +0300

    Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -44,6 +44,59 @@ class TestCourses:
             min_score="10"
         )
 
+    def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+
+        create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
+        create_course_page.create_course_form.fill(
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10"
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title="Playwright",
+            estimated_time="2 weeks",
+            max_score="100",
+            min_score="10"
+        )
+
+        courses_list_page.course_view.menu.menu_button.click()
+        courses_list_page.course_view.menu.edit_menu_item.click()
+
+        create_course_page.create_course_form.fill(
+            title="Python",
+            estimated_time="4 weeks",
+            description="Python",
+            max_score="200",
+            min_score="20"
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title="Python",
+            estimated_time="4 weeks",
+            max_score="200",
+            min_score="20"
+        )
+
+        # если действительно надо проверить поле description
+        courses_list_page.course_view.menu.menu_button.click()
+        courses_list_page.course_view.menu.edit_menu_item.click()
+
+        create_course_page.create_course_form.check_visible(
+            title="Python",
+            estimated_time="4 weeks",
+            description="Python",
+            max_score="200",
+            min_score="20"
+        )
+
     def test_empty_courses_list(self, courses_list_page: CoursesListPage):
         courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
